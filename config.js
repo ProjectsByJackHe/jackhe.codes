00cd7889b7761d81bec60ec1b229372f3c0d00b7
Jack He <jackhe2345@gmail.com>
2021-08-18 17:54:24 -0700

    change default port listens

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV != 'production') require('dotenv').load()
 
 module.exports = {
 	APPNAME: process.env.APPNAME || 'App Template',
-    PORT: process.env.PORT || 7777,
+    PORT: process.env.JACKHE_PORT || 5000,
     DEVMODE: (process.env.NODE_ENV != 'production'),
     AIRTABLE: {
     	API_KEY: process.env.AIRTABLE_API_KEY,
